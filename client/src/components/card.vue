b01ce6fbbe7b004e1e5ef98d642adee85a067277
Nightmarlin <lmiller81694@gmail.com>
2019-07-18 16:57:53 +0100

    Create card.vue

---
+++ client/src/components/card.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-flex>
+
+    <!--  -->
+
+  </v-flex>
+</template>
+
+<script>
+export default {
+  name: 'card',
+  props: {
+    initBlackCardType: Object,
+    // Standard - Pick 2 - Draw 2 Pick 3
+    initIsBlack: Boolean,
+    initText: String
+  },
+  data () {
+    return {
+      blackCardType: this.props.initBlackCardType,
+      isBlack: this.props.initIsBlack,
+      text: this.props.text,
+      isSelected: false,
+      isHovered: false
+    }
+  },
+  computed: {},
+  methods: {},
+  watch: {}
+}
+</script>
+
+<style>
+.cardBackground {
+  background-color: darkslategray;
+}
+</style>

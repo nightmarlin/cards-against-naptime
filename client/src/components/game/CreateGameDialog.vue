e3a871759ec278e3550a947aa64f169648f41b01
Nightmarlin <lmiller81694@gmail.com>
2019-08-08 17:49:31 +0100

    Testing in progress

---
+++ client/src/components/game/CreateGameDialog.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-dialog
+    v-model="showDialog"
+    persistent
+  >
+
+    <template v-slot:activator="{ on }">
+      <v-btn>
+        Open
+      </v-btn>
+    </template>
+
+    <v-card>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: 'createGameDialog',
+  data () {
+    return {
+      //
+    }
+  },
+  props: {
+    showDialog: {
+      type: Boolean,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- client/src/views/Testing.vue
+++ client/src/views/Testing.vue
@@ -1,40 +1,20 @@
 <template>
   <v-layout justify-center align-center>
     <v-flex>
-      <can-logo :height="256" :width="256" />
+      <create-game-dialog :showDialog="showDialog" />
     </v-flex>
   </v-layout>
 </template>
 
 <script>
-import canLogo from '../components/app/CanLogo'
+import createGameDialog from '../components/game/CreateGameDialog'
 export default {
   components: {
-    canLogo
+    createGameDialog
   },
   data () {
     return {
-      testServer: {
-        author: 'Marlin',
-        accentColor: '#777777',
-        decks: [
-          { name: 'Default', description: 'Cood old CAN', id: 1, cards: { black: 50, blank: 2, white: 200 } },
-          { name: 'Radix 8', description: 'Extra techy for those who need it', id: 2234, cards: { black: 25, blank: 0, white: 100 } },
-          { name: 'Cards against kittens', description: 'aww', id: 543, cards: { black: 40, blank: 10, white: 150 } },
-          { name: 'UK expansion pack 1', description: 'More cards for more fun', id: 222, cards: { black: 10, blank: 2, white: 100 } }
-        ],
-        description: 'For funsies',
-        maxMemberCount: '12',
-        memberCount: '6',
-        name: 'Test Server',
-        password: false,
-        spectators: {
-          enabled: true,
-          maxSpectatorCount: 10,
-          spectatorCount: 4
-        },
-        url: 'huehueheuhue'
-      }
+      showDialog: true
     }
   },
   methods: { },
